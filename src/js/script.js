c0e23f2583e6951e2a5a5bfdc45690fb220f90a1
Anton Soldatov <turbolet85@gmail.com>
2022-08-09 15:29:04 +0200

    complete two sections

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+
+//menu activation
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const closeElem = document.querySelector('.menu__close');
+const overlay = document.querySelector('.menu__overlay');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+window.addEventListener("keydown", (e) => {
+    if (e.key == 'Escape') {
+                menu.classList.remove('active');
+    }
+});
